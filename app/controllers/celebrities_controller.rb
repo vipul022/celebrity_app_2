7b4bebe0f5778ec649bc85cd47eb05752d6f842e
vipul022 <vipul.seth022@gmail.com>
2020-06-16 16:21:06 +1000

    initial commit

---
+++ app/controllers/celebrities_controller.rb
@@ -0,0 +1,35 @@
+ class CelebritiesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :setup_data
+    
+    #Show all celebrities
+    def index
+    end
+    
+    #Show a single celebrity
+    def show
+    end
+    
+    #Create a new celebrity
+    def create
+    end
+    
+    #Update a celebrity
+    def update
+    end
+    
+    #Remove a celebrity
+    def destroy
+    end
+
+    private
+    def setup_data
+        session[:celebrities] = [
+            { "name" => "Lindsay Lohan", "notability" => "Parent Trap" },
+            { "name" => "Adam Sandler", "notability" => "Big Daddy" },
+            { "name" => "Rob Schnider", "notability" => "Adam Sandler" }
+        ] unless session[:celebrities]
+    
+        @celebrities = session[:celebrities]
+    end
+end
